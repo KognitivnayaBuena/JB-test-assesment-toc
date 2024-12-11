a102b4b7b025accd39cd696c8604a00aa3163baf
Алена <mesherla001@gmail.com>
2024-12-11 17:30:04 +0100

    demo without server and data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import TableOfContent from './components/TableOfContent';
-import { TOCData } from './components/TableOfContent/types';
-import useFetch from './hooks/useFetch';
+// import { TOCData } from './components/TableOfContent/types';
+// import useFetch from './hooks/useFetch';
+
+import demoData from "../serverData/TOCData.json";
 
 const App: React.FC = () => {
-  const [data, loading, error] = useFetch<TOCData>('http://localhost:3000/serverData/TOCData.json');
+  // const [data, loading, error] = useFetch<TOCData>('http://localhost:3000/serverData/TOCData.json');
 
   return (
-    <TableOfContent data={data} loading={loading} error={error} />
+    <TableOfContent data={demoData} loading={false} error={null} />
   );
 };
 
